99109eb1ea258925846b7ed5c5fae15f5454a69a
Gustavo Castaneda <gustavocastaneda2323@gmail.com>
2020-10-03 11:23:53 -0700

    Task 1 Workshop Week 5

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Breadcrumb, BreadcrumbItem, Card, CardBody, CardHeader, Media } from 'reactstrap';
+import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 import { Link } from 'react-router-dom';
 
 function RenderPartner({ partner }) {
     if (partner) {
         return (
             <React.Fragment>
-                <Media object={true} src={partner.image} alt={partner.name} width={150} />
+                <Media object={true} src={baseUrl + partner.image} alt={partner.name} width={150} />
                 <Media body={true} className="ml-5 mb-4">
                     <Media heading={true}>
                         {partner.name}
@@ -20,15 +22,35 @@ function RenderPartner({ partner }) {
     return <div />;
 }
 
-function About(props) {
-
-    const partners = props.partners.map(partner => {
+function PartnerList(props){
+    const partners = props.partners.partners.map(partner => {
         return (
             <Media>
                 <RenderPartner partner={partner}/>
             </Media>
         );
     });
+    
+    if (props.partners.isLoading) {
+        return <Loading />;
+    }
+    
+    if (props.partners.errMess) {
+        return (
+            <div className="col">
+                <h4>{props.partners.errMess}</h4>
+            </div>
+        );
+    }
+
+    return (
+        <div className="col mt-4">
+            <Media list>{partners}</Media>
+        </div>
+    );
+}
+
+function About(props) {
 
     return (
         <div className="container">
@@ -82,11 +104,7 @@ function About(props) {
                 <div className="col-12">
                     <h3>Community Partners</h3>
                 </div>
-                <div className="col mt-4">
-                    <Media list>
-                        {partners}
-                    </Media>
-                </div>
+                <PartnerList partners={props.partners}/>
             </div>
         </div>
     );
